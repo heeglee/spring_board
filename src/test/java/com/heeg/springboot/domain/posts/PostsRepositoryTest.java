75d5a1a036dd08f994154b28e299c748c5560528
heeglee <heeg.aver@gmail.com>
2020-01-06 04:46:39 +0900

    2020/1/6 CRUD 구현

---
+++ src/test/java/com/heeg/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.heeg.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void articleSaveLoad() {
+        // given
+        String title = "TEST";
+        String content = "TEST CONTENT";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("heeg").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void baseTimeEntityTest() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2020, 1, 6, 1, 20, 0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>> createdDate=" + posts.getCreatedDate());
+        System.out.println(">>>> modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
